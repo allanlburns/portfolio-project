a6ae24ecab8ef2fe139ec2b81103fa60c9fb3be8
Allan Burns <allanlburns@yahoo.com>
2019-03-16 11:22:16 -0400

    Prepare for Digitalocean deployment following steps from Brad Traversy Django course.

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,5 +123,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
