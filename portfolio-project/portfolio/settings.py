9d271f9e7a03713a22448ea0b0a27abc3231aa0e
Allan Burns <allanlburns@yahoo.com>
2019-03-16 11:46:44 -0400

    Import local_settings to settings.py.

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -1,5 +1,4 @@
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,6 +121,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# Configure Django App for Heroku.
-
-django_heroku.settings(locals())
+try:
+    from .local_settings import *
+except ImportError:
+    pass
